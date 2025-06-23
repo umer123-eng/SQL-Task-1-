46193c244748aad90fd4839cde74d1beb15304e7
Umer <umer2006official@gmail.com>
2025-06-23 17:08:04 +0530

    Add files via upload

--- data.sql(for data).sql
+++ data.sql(for data).sql
@@ -1,23 +1,27 @@
+
+-- add users in user table 
 INSERT INTO users (name, email, password) VALUES
 ('Umer Mansuri', 'umer@example.com', 'pass123'),
 ('Ali Khan', 'ali@example.com', 'pass456');
 
-
+-- add categories 
 INSERT INTO categories (name) VALUES
 ('Electronics'),
 ('Clothing'),
 ('Books');
 
-
+-- add products details
 INSERT INTO products (name, description, price, category_id) VALUES
 ('Laptop', 'Dell i5 Laptop', 55000.00, 1),
 ('T-Shirt', 'Cotton T-shirt', 500.00, 2),
 ('Java Book', 'Learn Java Programming', 799.00, 3);
 
-
+-- add or receive order for the customer
 INSERT INTO orders (user_id, status) VALUES
 (1, 'Shipped'),
 (2, 'Pending');
+
+delete from order_items where quantity='2,2,1'; 
 
 
 INSERT INTO order_items (order_id, product_id, quantity) VALUES
@@ -30,9 +34,15 @@ INSERT INTO payments (order_id, amount, payment_method) VALUES
 (1, 56598.00, 'Credit Card'),
 (2, 500.00, 'Cash on Delivery');
 
+-- display the all added rows from diff tables
 SELECT * FROM users;
 SELECT * FROM orders;
 SELECT * FROM order_items;
 SELECT * FROM payments;
 SELECT * FROM categories;
 SELECT * FROM products;
+
+-- delete the some specific id in the table
+DELETE FROM order_items WHERE order_id = 1;
+
+
--- schema(for tables).sql
+++ schema(for tables).sql
@@ -1,33 +1,37 @@
 
-
+-- Create users table
 CREATE TABLE users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100) UNIQUE,
-    password VARCHAR(100),
+    user_id INT AUTO_INCREMENT PRIMARY KEY, -- use for unique id for each user
+    name VARCHAR(100), -- user
+    email VARCHAR(100) UNIQUE, -- user email-id
+    password VARCHAR(100), -- of them unique password
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-desc users;
+alter table users add(phoneNo  numeric(10));
+desc users; -- display the table 
 
+-- Create categories table
 CREATE TABLE categories (
-    category_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100)
+    category_id INT AUTO_INCREMENT PRIMARY KEY,  -- use for unique id for each user
+    name VARCHAR(100) 
 );
 
-desc categories;
+desc categories; 
 
+-- Create products table
 CREATE TABLE products (
-    product_id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT AUTO_INCREMENT PRIMARY KEY, -- use for unique id for each user 
     name VARCHAR(100),
     description TEXT,
     price DECIMAL(10,2),
     category_id INT,
-    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+    FOREIGN KEY (category_id) REFERENCES categories(category_id) -- as a use for reference
 );
 
 desc products;
 
+-- Create order table
 CREATE TABLE orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
@@ -36,6 +40,7 @@ CREATE TABLE orders (
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
+desc orders;
 
 CREATE TABLE order_items (
     order_item_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -46,6 +51,7 @@ CREATE TABLE order_items (
     FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
 
+desc order_items;
 
 CREATE TABLE payments (
     payment_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -56,3 +62,4 @@ CREATE TABLE payments (
     FOREIGN KEY (order_id) REFERENCES orders(order_id)
 );
 
+desc payments;
